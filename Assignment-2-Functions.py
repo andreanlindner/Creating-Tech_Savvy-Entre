e9b4d392e03a40ae0c0d65595e15386949a8b1af
andreanlindner <31372872+andreanlindner@users.noreply.github.com>
2020-03-21 14:31:44 -0400

    created function
    Doesn't work fully, but this is my quadratic formula function

---
+++ Assignment-2-Functions.py
@@ -0,0 +1,21 @@
+import math
+
+def quadratic(a,b,c):
+    """
+    takes in a, b, and c to solve for x using the quadratic function (ax^2 + bx + c = 0)
+
+    a = an int
+    b = an int
+    c = an int
+    """
+
+    part1= int((-b)/(2*a))
+    sqroot = int((b**2) - (4*a*c))
+    part2 = math.sqrt(sqroot)/(2*a)
+    print("The values of x are "+ (part1-part2) + " and "+ (part1+part2)+ ".")
+
+def main():
+    return quadratic(2,4,5)
+
+if __name__ == '__main__':    
+    main()
